81dafb9c1a7d7c9d37b6b8ff4e4afd77789b0cf6
joey <50106036+joeyyyey@users.noreply.github.com>
2022-03-25 16:43:34 +0800

    update train.py cal accuracy

--- problem_1/train.py
+++ problem_1/train.py
@@ -5,11 +5,12 @@ from torch.utils.data import DataLoader
 import numpy as np
 import torch
 from torch import nn
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 from augmentation import RandomCrop, RandomRotFlip, ToTensor
 
 from model import UNet  # RandomRotation, RandomVerticalFlip, RandomHorizontalFlip, RandomCrop
-from loss import DiceLoss
+from loss import DiceLoss, dice_loss
 
 # Specify patch size for cropping
 patch_size = (112, 112, 80)
@@ -32,7 +33,7 @@ if __name__ == '__main__':
     use_cuda = True  # added
 
     # model initialization
-    model = UNet  # None
+    model = UNet()  # None
 
     train_dst = LAHeart(split='train', transform=transform)
     # test_dst = LAHeart(split='test', transform=None)  # commented out
@@ -55,15 +56,17 @@ if __name__ == '__main__':
 
     # loss function
     # criterion = nn.CrossEntropyLoss()
-#     criterion = DiceLoss()
+    criterion = DiceLoss()
 
     # optimizer
     Lr = 0.01
-    optimizer = torch.optim.SGD(model.parameters(), lr=Lr)  # None
-    # optimizer = torch.optim.Adam(model.parameters(),lr=Lr)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=Lr)  # None
+    optimizer = torch.optim.Adam(model.parameters(),lr = 0.001)
 
     loss_list = []  # added
     acc_train_list = []  # added
+    total = 0 # added
+
 
     for epoch in range(max_epoch):
         running_loss = 0.0
@@ -88,11 +91,11 @@ if __name__ == '__main__':
             outputs = model(images)
 
             # calculate loss
-            loss_seg = F.cross_entropy(outputs, labels)
-            outputs_soft = F.softmax(outputs, dim=1)
-            loss_seg_dice = dice_loss(outputs_soft[:, 1, :, :, :], labels == 1)
-            loss = 0.5*(loss_seg+loss_seg_dice)
-#             loss = criterion(outputs, labels)
+            # loss_seg = F.cross_entropy(outputs, labels)
+            # outputs_soft = F.softmax(outputs, dim=1)
+            # loss_seg_dice = dice_loss(outputs_soft[:, 1, :, :, :], labels == 1)
+            # loss = 0.5*(loss_seg+loss_seg_dice)
+            loss = criterion(outputs, labels)
 
             # backward and optimize parameters
             loss.backward()
@@ -100,12 +103,22 @@ if __name__ == '__main__':
 
             pred = torch.argmax(outputs, dim=1)
             running_loss += loss.item()
-            running_correct += torch.sum(pred == labels)
+            total += outputs.size(0)
+            # running_correct += torch.sum(pred == labels)
 
         # record loss, accuracy
         loss = running_loss / len(train_dst)
         loss_list.append(loss)
-        acc_train = running_correct / len(train_dst)
+        running_correct = torch.sum(pred == labels)
+        acc_train = running_correct / total
         acc_train_list.append(acc_train.item())
+
+        print("Loss {:.4f}, Train Accuracy {:.4f}%".format(
+            loss,
+            acc_train,
+        ))
+
+        print(len(train_dst))
+        print(running_correct)
 
         # pass
